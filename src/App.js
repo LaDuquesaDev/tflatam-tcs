6cfe80c45bb092cbd1848fba4084691633d77f5a
Yulimar Duque <yduquedealba@gmail.com>
2022-08-22 15:04:44 -0500

    Ruteo inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './views/Login';
+import Register from './views/Register';
+import Profile from './views/Profile';
+import NotFoundPage from './views/NotFoundPage';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Profile" element={<Profile />} />
+          {/* <Route path="/chef" element={<Chef />} />
+          <Route path="/boss" element={<Administrator />} /> */}
+          <Route path="*" element={<NotFoundPage />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
