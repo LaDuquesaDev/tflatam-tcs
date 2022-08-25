ed752056f52c924f50a8e2073c29f744fda2e302
Amaya De Esesarte Díaz González <ADEDG@MacBook-Air-de-Macintosh.local>
2022-08-25 11:13:23 -0500

    corrige estilos

--- src/views/Comments.js
+++ src/views/Comments.js
@@ -6,28 +6,28 @@ import Footer from '../components/Footer';
 
 
 export const Comments = () => {
-    return (
-        <div className='comments-principal'>
-            <Header/>
-            <div>
-                <h2>Has llegado a tu destino</h2>
-                <p>Como evaluarías tu viaje con Dariana?</p>
-                <div>
-                    <img className="" src={star} alt='Estrella'></img>
-                    <img className="" src={star} alt='Estrella'></img>
-                    <img className="" src={star} alt='Estrella'></img>
-                    <img className="" src={star} alt='Estrella'></img>
-                    <img className="" src={star} alt='Estrella'></img>
-                </div>
-                <p>Déjale un mensaje a Dariana</p>
-                <input type="text" name="name" className="" />
-                <EmergencyButton/>
-                <p>Evalúa a tus acompañantes</p>
-                <AccordionC/>
-                <button className='comments-button'>Enviar</button>
-                <EmergencyButton/>
-            </div>
-            <Footer/>
+  return (
+    <div className='comments-principal'>
+      <Header />
+      <div>
+        <h2>Has llegado a tu destino</h2>
+        <p>Como evaluarías tu viaje con Dariana?</p>
+        <div>
+          <img className="" src={star} alt='Estrella'></img>
+          <img className="" src={star} alt='Estrella'></img>
+          <img className="" src={star} alt='Estrella'></img>
+          <img className="" src={star} alt='Estrella'></img>
+          <img className="" src={star} alt='Estrella'></img>
         </div>
-    )
+        <p>Déjale un mensaje a Dariana</p>
+        <input type="text" name="name" className="" />
+        <EmergencyButton />
+        <p>Evalúa a tus acompañantes</p>
+        <AccordionC />
+        <button className='comments-button'>Enviar</button>
+        <EmergencyButton />
+      </div>
+      <Footer />
+    </div>
+  )
 }
